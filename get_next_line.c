eb902798c812b468338a3f6042b0b7d4aadc596d
Fiona LEITZ <fleitz@e1r6p8.42.fr>
2021-12-07 17:12:06 +0100

    il fonctionne pas mal !

--- get_next_line.c
+++ get_next_line.c
@@ -6,10 +6,11 @@
 /*   By: fleitz <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/04 13:28:31 by fleitz            #+#    #+#             */
-/*   Updated: 2021/12/06 17:59:48 by fleitz           ###   ########.fr       */
+/*   Updated: 2021/12/07 17:10:58 by fleitz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../libftgithub/libft.h"
 #include "get_next_line.h"
 #include <stdlib.h>
 #include <string.h>
@@ -18,50 +19,14 @@
 /*
 char	*get_next_line(int fd)
 {
-	char	*str;
-	char	*save;
-	char	*buf;
-	size_t	end;
-
-	buf = malloc(sizeof(char) * BUFFER_SIZE);
-	if (buf == NULL)
-		return (NULL);
-	end = 0;
-	str = malloc(sizeof(char));
-	if (str == NULL)
-	{
-		free(buf);
-		return (0);
-	}
-	while (read(fd, buf, (BUFFER_SIZE - 1)) != 0)
-	{
-		if (strchr(buf, '\n') != NULL)
-		{
-			while (buf[end] != '\n')
-				end++;
-			strncat(str, buf, (end + 1));
-			save
-			free(buf);
-			return (str);
-		}
-		strcat(str, buf);
-	}
-	free(buf);
-	return (str);
-}
-*/
-
-char	*get_next_line(int fd)
-{
 	static char	*buff;
 	char		*str;
-	char		*save;
 	size_t		end;
 
-	str2 = malloc(sizeof(char) * BUFFER_SIZE);
-	if (str2 == NULL)
+	str = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (str == NULL)
 		return (NULL);
-	str2[0] = '\0';
+	str[0] = '\0';
 	if (buff != NULL)
 	{
 		if (strchr(buff, '\n') != NULL)
@@ -81,7 +46,7 @@ char	*get_next_line(int fd)
 		free(str);
 		return (0);
 	}
-	while (read(fd, buff, (BUFFER_SIZE - 1) > 0))
+	while (read(fd, buff, (BUFFER_SIZE) > 0))
 	{
 		if (strchr(buff, '\n') != NULL)
 		{
@@ -95,5 +60,79 @@ char	*get_next_line(int fd)
 		strcat(str, buff);
 	}
 	free(buff);
+	return (NULL);
+}*/
+
+char	*get_next_line(int fd)
+{
+	static char	buff[BUFFER_SIZE + 1];
+	char		*str;
+	char		*save;
+	char		*save2;
+	size_t		end;
+	ssize_t		rd;
+
+	str = NULL;
+	if (buff[0] != '\0')
+	{
+		if (strchr(buff, '\n') != NULL)
+		{
+			end = 0;
+			while (buff[end] != '\n')
+				end++;
+			if (str == NULL)
+				str = strndup(buff, (end + 1));
+			else
+			{
+				save = ft_strjoin(str, buff);
+				save[ft_strlen(str) + end + 1] = '\0';
+				free(str);
+			memcpy(buff, &buff[end + 1], (ft_strlen(&buff[end + 1]) + 1));
+				return (save);
+			}
+			memcpy(buff, &buff[end + 1], (ft_strlen(&buff[end + 1]) + 1));
+			return (str);
+		}
+		str = strdup(buff);
+	}
+	while ((rd = read(fd, buff, BUFFER_SIZE)) == BUFFER_SIZE)
+	{
+		if (strchr(buff, '\n') != NULL)
+		{
+			end = 0;
+			while (buff[end] != '\n')
+				end++;
+			if (str == NULL)
+				str = strndup(buff, (end + 1));
+			else
+			{
+				save2 = malloc(sizeof(char) * BUFFER_SIZE + 1);
+				save2 = strndup(buff, end + 1);
+				save = ft_strjoin(str, save2);
+				free(save2);
+				free(str);
+				memcpy(buff, &buff[end + 1], (ft_strlen(&buff[end + 1]) + 1));
+				return (save);
+			}
+			memcpy(buff, &buff[end + 1], (ft_strlen(&buff[end + 1]) + 1));
+			return (str);
+		}
+		if (str == NULL)
+			str = strdup(buff);
+		else
+		{
+			save = ft_strjoin(str, buff);
+			free(str);
+			str = strdup(save);
+			free(save);
+		}
+	}
+	if (rd > 0)
+	{
+		buff[rd] = '\0';
+		save = ft_strjoin(str, buff);
+		free(str);
+		return (save);
+	}
 	return (NULL);
 }
