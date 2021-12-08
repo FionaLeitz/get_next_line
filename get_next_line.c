2854bb0e2906cc64da498b25fe10993f2eb3ffbf
Fiona LEITZ <fleitz@e1r6p8.42.fr>
2021-12-08 17:49:05 +0100

    on est pas mal

--- get_next_line.c
+++ get_next_line.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   ft_gnl2.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fleitz <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/04 13:28:31 by fleitz            #+#    #+#             */
-/*   Updated: 2021/12/07 17:10:58 by fleitz           ###   ########.fr       */
+/*   Created: 2021/12/08 09:54:23 by fleitz            #+#    #+#             */
+/*   Updated: 2021/12/08 17:45:22 by fleitz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,123 +16,87 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdio.h>
-/*
-char	*get_next_line(int fd)
-{
-	static char	*buff;
-	char		*str;
-	size_t		end;
 
-	str = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+char	*ft_n_buffcpy(char *buff, char *str)
+{
+	size_t	end;
+	size_t	size;
+	char	*save;
+	char	*save2;
+
+	end = 0;
+	while (buff[end] != '\0' && buff[end] != '\n')
+		end++;
+	end++;
 	if (str == NULL)
-		return (NULL);
-	str[0] = '\0';
-	if (buff != NULL)
+		str = strndup(buff, end);
+	else
 	{
-		if (strchr(buff, '\n') != NULL)
-		{
-			end = 0;
-			while (buff[end] != '\n')
-				end++;
-			str = strndup(buff, (end + 1));
-			buff = &buff[end + 1];
-			return (str);
-		}
-		strcat(str, buff);
-	}
-	buff = malloc(sizeof(char) * BUFFER_SIZE);
-	if (buff == NULL)
-	{
+		save2 = strndup(buff, end);
+		save = ft_strjoin(str, save2);
 		free(str);
-		return (0);
+		str = ft_strdup(save);
+		free(save);
+		free(save2);
 	}
-	while (read(fd, buff, (BUFFER_SIZE) > 0))
+	size = BUFFER_SIZE - end;
+	ft_memcpy(buff, &buff[end], size);
+	buff[size] = '\0';
+	return (str);
+}
+
+char	*ft_buffcpy(char *buff, char *str)
+{
+	char	*save;
+
+	if (str == NULL)
 	{
-		if (strchr(buff, '\n') != NULL)
-		{
-			end = 0;
-			while (buff[end] != '\n')
-				end++;
-			strncat(str, buff, (end + 1));
-			buff = &buff[end + 1];
-			return (str);
-		}
-		strcat(str, buff);
+		str = ft_strdup(buff);
+		return (str);
 	}
-	free(buff);
-	return (NULL);
-}*/
+	save = ft_strjoin(str, buff);
+	free(str);
+	ft_bzero(buff, (BUFFER_SIZE + 1));
+	return (save);
+}
 
 char	*get_next_line(int fd)
 {
 	static char	buff[BUFFER_SIZE + 1];
 	char		*str;
-	char		*save;
-	char		*save2;
-	size_t		end;
 	ssize_t		rd;
 
 	str = NULL;
-	if (buff[0] != '\0')
+	while (1)
 	{
-		if (strchr(buff, '\n') != NULL)
+		if (buff[0] == '\0' && buff[1] == 1)
 		{
-			end = 0;
-			while (buff[end] != '\n')
-				end++;
-			if (str == NULL)
-				str = strndup(buff, (end + 1));
-			else
+			return (NULL);
+		}
+		if (buff[0] == '\0')
+		{
+			rd = read(fd, buff, BUFFER_SIZE);
+			if (rd == -1)
+				return (NULL);
+			if (rd < BUFFER_SIZE)
 			{
-				save = ft_strjoin(str, buff);
-				save[ft_strlen(str) + end + 1] = '\0';
-				free(str);
-			memcpy(buff, &buff[end + 1], (ft_strlen(&buff[end + 1]) + 1));
-				return (save);
+				if (ft_strchr(buff, '\n') != NULL)
+				{
+					str = ft_n_buffcpy(buff, str);
+					return (str);
+				}
+				str = ft_buffcpy(buff, str);
+				buff[1] = 1;
+				return (str);
 			}
-			memcpy(buff, &buff[end + 1], (ft_strlen(&buff[end + 1]) + 1));
+		}
+		if (ft_strchr(buff, '\n') != NULL)
+		{
+			str = ft_n_buffcpy(buff, str);
 			return (str);
 		}
-		str = strdup(buff);
-	}
-	while ((rd = read(fd, buff, BUFFER_SIZE)) == BUFFER_SIZE)
-	{
-		if (strchr(buff, '\n') != NULL)
-		{
-			end = 0;
-			while (buff[end] != '\n')
-				end++;
-			if (str == NULL)
-				str = strndup(buff, (end + 1));
-			else
-			{
-				save2 = malloc(sizeof(char) * BUFFER_SIZE + 1);
-				save2 = strndup(buff, end + 1);
-				save = ft_strjoin(str, save2);
-				free(save2);
-				free(str);
-				memcpy(buff, &buff[end + 1], (ft_strlen(&buff[end + 1]) + 1));
-				return (save);
-			}
-			memcpy(buff, &buff[end + 1], (ft_strlen(&buff[end + 1]) + 1));
-			return (str);
-		}
-		if (str == NULL)
-			str = strdup(buff);
-		else
-		{
-			save = ft_strjoin(str, buff);
-			free(str);
-			str = strdup(save);
-			free(save);
-		}
-	}
-	if (rd > 0)
-	{
-		buff[rd] = '\0';
-		save = ft_strjoin(str, buff);
-		free(str);
-		return (save);
+		str = ft_buffcpy(buff, str);
+		buff[0] = '\0';
 	}
 	return (NULL);
 }
