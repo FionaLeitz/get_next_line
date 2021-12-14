2dda498b35e9891788fe5739ee648f9732a4ad8d
Fiona LEITZ <fleitz@e1r6p8.42.fr>
2021-12-14 09:45:02 +0100

    all clear

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,7 +6,7 @@
 /*   By: fleitz <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/06 10:02:14 by fleitz            #+#    #+#             */
-/*   Updated: 2021/12/09 14:43:00 by fleitz           ###   ########.fr       */
+/*   Updated: 2021/12/14 09:42:58 by fleitz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,11 @@
 char	*get_next_line(int fd);
 char	*ft_buffcpy(char *buff, char *str);
 char	*ft_n_buffcpy(char *buff, char *str);
-size_t	ft_strlen(const char *s);
+char	*ft_else(char *buff, char *str, size_t n);
 char	*ft_strndup(const char *s1, size_t n);
 char	*ft_strjoin(char const *s1, char const *s2);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 char	*ft_strchr(const char *s, int c);
-char	*strcat(char *s1, const char *s2);
 int		ft_stop(char *buff, ssize_t rd);
 
 #endif
