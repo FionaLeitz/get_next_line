af53d437630fc9b6b9f329823fae32b599713939
Fiona LEITZ <fleitz@e2r5p20.clusters.42paris.fr>
2023-04-26 10:42:05 +0200

    clean .h

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 char	*ft_else(char *buff, char *str, size_t end)
 {
